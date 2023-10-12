b7694c55b83302a73eef96ae2b8e28c68172f144
Tim <115729319+Timasoska@users.noreply.github.com>
2023-10-12 18:39:27 +0300

    Add files via upload

---
+++ Java_practise_15/src/main/java/com/example/Java_practise_15/JavaPractise15Application.java
@@ -0,0 +1,13 @@
+package com.example.Java_practise_15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPractise15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPractise15Application.class, args);
+	}
+
+}
---
+++ Java_practise_15/src/test/java/com/example/Java_practise_15/JavaPractise15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java_practise_15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPractise15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
